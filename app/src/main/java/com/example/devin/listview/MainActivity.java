e4a27a3e6e47b53e9a5b6f24f9ac07877dc9c304
Devin Alexander <devinmalexander@gmail.com>
2019-03-03 16:24:34 -0600

    Changes related to ListView TNB Tutorial

--- app/src/main/java/com/example/devin/listview/MainActivity.java
+++ app/src/main/java/com/example/devin/listview/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.devin.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String foods[] = {"Bacon", "Ham", "Tuna", "Candy", "Meatball", "Potato"};
+        ListAdapter devinsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        ListView devinsListView = (ListView) findViewById(R.id.devinsListView);
+        devinsListView.setAdapter(devinsAdapter);
+
+        devinsListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                        String food = String.valueOf(adapterView.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
